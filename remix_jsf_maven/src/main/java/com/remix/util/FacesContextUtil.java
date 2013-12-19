b80f4048b513d4b18807956f9d5e45a20b4ced8e
Remix <Remix@Computer-PC>
2013-12-19 03:08:08 -0200

    Iniciando o projeto Web Remix.

---
+++ remix_jsf_maven/src/main/java/com/remix/util/FacesContextUtil.java
@@ -0,0 +1,17 @@
+package com.remix.util;
+
+import javax.faces.context.FacesContext;
+import org.hibernate.Session;
+
+public class FacesContextUtil {
+
+    private static final String HIBERNATE_SESSION = "hibernate_sesison";
+
+    public static void setRequestSession(Session session) {
+        FacesContext.getCurrentInstance().getExternalContext().getRequestMap().put(HIBERNATE_SESSION, session);
+    }
+
+    public static Session getRequestSession() {
+        return (Session) FacesContext.getCurrentInstance().getExternalContext().getRequestMap().get(HIBERNATE_SESSION);
+    }
+}
