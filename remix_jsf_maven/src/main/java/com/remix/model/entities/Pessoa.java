8ded2c089defb4bde331ccd3400bf50d7ba7a0f6
Aluno <Aluno@LF102M06>
2013-12-20 19:08:40 -0200

    Finalização das entidades.

--- remix_jsf_maven/src/main/java/com/remix/model/entities/Pessoa.java
+++ remix_jsf_maven/src/main/java/com/remix/model/entities/Pessoa.java
@@ -4,9 +4,12 @@ import java.io.Serializable;
 import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import org.hibernate.annotations.ForeignKey;
@@ -36,10 +39,15 @@ public class Pessoa implements Serializable {
     @Temporal(javax.persistence.TemporalType.DATE)
     private Date dataDeCadastro;
 
-    @ManyToOne(optional=false)
+    @OneToOne(mappedBy = "pessoa", fetch = FetchType.LAZY)
+    @ForeignKey(name = "EnderecoPessoa")
+    private Endereco endereco;
+
+    @ManyToOne(optional = false)
     @ForeignKey(name = "PessoaSexo")
-    private Pessoa pessoa;
-    
+    @JoinColumn(name = "IdSexo", referencedColumnName = "IdSexo")
+    private Sexo sexo;
+
     public Pessoa() {
     }
 
@@ -97,6 +105,22 @@ public class Pessoa implements Serializable {
 
     public void setDataDeCadastro(Date dataDeCadastro) {
         this.dataDeCadastro = dataDeCadastro;
+    }
+
+    public Sexo getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Sexo sexo) {
+        this.sexo = sexo;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
     }
 
     @Override
