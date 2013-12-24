22a9ba707f584325e08ad6a7abb149989ff3fcaf
Remix <Remix@Computer-PC>
2013-12-24 14:02:16 -0200

    Término do MB pessoa, alteração do tema e inicio do cadastro de pessoas.

---
+++ remix_jsf_maven/src/main/java/com/remix/controller/MbPessoa.java
@@ -0,0 +1,108 @@
+package com.remix.controller;
+
+import com.remix.model.dao.HibernateDAO;
+import com.remix.model.dao.InterfaceDAO;
+import com.remix.model.entities.Endereco;
+import com.remix.model.entities.Pessoa;
+import com.remix.util.FacesContextUtil;
+import java.io.Serializable;
+import java.util.List;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@SessionScoped
+public class MbPessoa implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Pessoa pessoa = new Pessoa();
+    private Endereco endereco = new Endereco();
+    private List<Pessoa> pessoas;
+    private List<Endereco> enderecos;
+
+    private InterfaceDAO<Pessoa> pessoaDAO() {
+        InterfaceDAO<Pessoa> pessoaDAO = new HibernateDAO<Pessoa>(Pessoa.class, FacesContextUtil.getRequestSession());
+        return pessoaDAO;
+    }
+
+    private InterfaceDAO<Endereco> enderecoDAO() {
+        InterfaceDAO<Endereco> enderecoDAO = new HibernateDAO<Endereco>(Endereco.class, FacesContextUtil.getRequestSession());
+        return enderecoDAO;
+    }
+
+    public String limpPessoa() {
+        pessoa = new Pessoa();
+        endereco = new Endereco();
+        return editPessoa();
+    }
+
+    public String editPessoa() {
+        return "/restrict/cadastrarpessoa.faces";
+    }
+
+    public String addPessoa() {
+        if (pessoa.getIdPessoa() == null || pessoa.getIdPessoa() == 0) {
+            insertPessoa();
+        }
+        else{
+            updatePessoa();
+        }
+        return null;
+    }
+
+    private void insertPessoa() {
+        pessoaDAO().save(pessoa);
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage(FacesMessage.SEVERITY_INFO, "Gravação efetuada com sucesso", ""));
+    }
+
+    private void updatePessoa() {
+        pessoaDAO().update(pessoa);
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage(FacesMessage.SEVERITY_INFO, "Atualização efetuada com sucesso", ""));
+    }
+    
+    public String deletePessoa(){
+        pessoaDAO().remove(pessoa);
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage(FacesMessage.SEVERITY_INFO, "Registro excluido com sucesso", ""));
+        return null;
+    }
+
+    public List<Pessoa> getPessoas() {
+        pessoas = pessoaDAO().getEntities();
+        return pessoas;
+    }
+
+    public void setPessoas(List<Pessoa> pessoas) {
+        this.pessoas = pessoas;
+    }
+
+    public List<Endereco> getEnderecos() {
+        enderecos = enderecoDAO().getEntities();
+        return enderecos;
+    }
+
+    public void setEnderecos(List<Endereco> enderecos) {
+        this.enderecos = enderecos;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+}
